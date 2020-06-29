a7b645e6c71937183bc23b605c4d61e372ef30c6
amlwin <amlwin.mm@gmail.com>
2020-06-29 19:45:10 +0630

    navigation setup with toolbar

--- app/src/main/java/com/amlwin/android/archsample/MainActivity.kt
+++ app/src/main/java/com/amlwin/android/archsample/MainActivity.kt
@@ -2,11 +2,23 @@ package com.amlwin.android.archsample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.amlwin.android.archsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host_fragment)
+        val appbarConfig = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appbarConfig)
     }
 }
